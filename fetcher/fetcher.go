26ef9712f2c271a2fcc686f13df2f80608607fc5
James Myers <jmyers@pivotal.io>
2015-12-18 12:15:49 -0800

    Allow LRPs to be sortable

--- fetcher/fetcher.go
+++ fetcher/fetcher.go
@@ -1,6 +1,8 @@
 package fetcher
 
 import (
+	"sort"
+
 	"github.com/cloudfoundry-incubator/bbs"
 	"github.com/cloudfoundry-incubator/bbs/models"
 )
@@ -16,7 +18,32 @@ type fetcher struct {
 type Data struct {
 	Domains []string
 	Tasks   []*models.Task
-	LRPs    map[string]*LRP
+	LRPs    LRPs
+}
+
+type LRPs map[string]*LRP
+
+func (l LRPs) SortedByProcessGuid() []*LRP {
+	var lrps []*LRP
+
+	for _, lrp := range l {
+		lrps = append(lrps, lrp)
+	}
+
+	sort.Sort(ByProcessGuid(lrps))
+	return lrps
+}
+
+func ByProcessGuid(lrps []*LRP) LRPsByProcessGuid {
+	return lrps
+}
+
+type LRPsByProcessGuid []*LRP
+
+func (l LRPsByProcessGuid) Len() int      { return len(l) }
+func (l LRPsByProcessGuid) Swap(i, j int) { l[i], l[j] = l[j], l[i] }
+func (l LRPsByProcessGuid) Less(i, j int) bool {
+	return l[i].Desired.ProcessGuid < l[j].Desired.ProcessGuid
 }
 
 type LRP struct {
